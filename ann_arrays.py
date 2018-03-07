21e9dd720f0e98a7ca377488faeebe3157abba02
MrGi94 <gramlich.andre@yahoo.de>
2018-03-07 23:43:29 +0100

    reworked weight transformation and added some test cases

--- ann_arrays.py
+++ ann_arrays.py
@@ -12,11 +12,12 @@ def flatten_weights(list_of_mats):
 
 
 class NeuralNet:
-    def __init__(self, weights, nr_of_input_nodes=12, hidden_layers=1, hidden_layer_nodes=6, nr_of_outputs=2, recurrence=False):
+    def __init__(self, weights, nr_of_input_nodes=12, hidden_layers=1,
+                 hidden_layer_nodes=6, nr_of_outputs=2, recurrence=False):
         self.nr_of_input_nodes = nr_of_input_nodes
         self.hidden_layers = hidden_layers
         self.hidden_layer_nodes = hidden_layer_nodes
-        self.nr_of_outputs = nr_of_outputs
+        self.nr_of_output_nodes = nr_of_outputs
         self.weights = weights  # One array of numbers, that contain the weights ordered
         self.recurrence = recurrence
         if recurrence:
@@ -26,19 +27,18 @@ class NeuralNet:
                 self.prev_out.append(0)
 
     def weights_as_mat(self):
-
         weights = []
-
+        split_count = self.nr_of_output_nodes
         # Initialize matrix from input layer to first layer
         if self.hidden_layers > 0:
-            w1 = np.empty([self.hidden_layer_nodes, self.nr_of_input_nodes])
+            w1 = np.empty([self.nr_of_input_nodes, self.hidden_layer_nodes])
+            split_count = self.hidden_layer_nodes
         else:  # If first layer is output layer
-            w1 = np.empty([self.nr_of_outputs, self.nr_of_input_nodes])
-
+            w1 = np.empty([self.nr_of_input_nodes, self.nr_of_output_nodes])
         start = 0
         for index, vector in enumerate(w1):
-            w1[index] = self.weights[start:start + self.nr_of_input_nodes]
-            start += self.nr_of_input_nodes
+            w1[index] = self.weights[start:start + split_count]
+            start += split_count
         weights.append(w1)
 
         if self.hidden_layers > 0:
@@ -51,19 +51,19 @@ class NeuralNet:
                 weights.append(wi)
 
             # Matrix from last hidden layer to output layer
-            wn = np.empty([self.hidden_layer_nodes, self.nr_of_outputs])
+            wn = np.empty([self.hidden_layer_nodes, self.nr_of_output_nodes])
             for index, vector in enumerate(wn):
-                wn[index] = self.weights[start:start + self.hidden_layer_nodes]
-                start += self.hidden_layer_nodes
+                wn[index] = self.weights[start:start + self.nr_of_output_nodes]
+                start += self.nr_of_output_nodes
             weights.append(wn)
         return weights
 
-    def forward_prop(self, inputs):
+    def forward_prop(self, _inputs):
         # Append last rounds outputs as input values if recurrence is enabled
+        inputs = list(_inputs)
         if self.recurrence:
             for out in self.prev_out:
                 inputs.append(out)
-
         output = inputs  # The inputs will be transformed to output through several matmuls
         # This is where we propagate through the layers
         for weights in self.weights_as_mat():
--- tests/ann_test.py
+++ tests/ann_test.py
@@ -2,7 +2,6 @@ import unittest
 import numpy as np
 import ann_arrays as nn
 import numpy.testing as npt
-import math
 
 __author__ = 'Olve Drageset'
 
@@ -10,18 +9,39 @@ __author__ = 'Olve Drageset'
 class TestANN(unittest.TestCase):
     def testTranslation(self):
         array = [0., 1., 2., 3., 4., 5., 6., 7.]
-        mat1 = [np.array([[0., 1., 2., 3.], [4., 5., 6., 7.]])]
+        mat1 = [np.array([[0., 1.], [2., 3.], [4., 5.], [6., 7.]])]
 
         net = nn.NeuralNet(weights=array, nr_of_input_nodes=4, hidden_layers=0, nr_of_outputs=2, recurrence=False)
         mat2 = net.weights_as_mat()
         for i in range(0, len(mat1)):
             npt.assert_almost_equal(mat1[i], mat2[i])
 
-    def testTranslation2(self):
-        array = [0., 1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11.]
-        mat1 = [np.array([[0., 1., 2., 3.], [4., 5., 6., 7.]]), np.array([[8., 9.], [10., 11.]])]
+    def testTranslationWithHiddenLayer(self):
+        array = [0., 1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12., 13.]
+        mat1 = [np.array([[0., 1.], [2., 3.], [4., 5.], [6., 7.]]), np.array([[8., 9., 10.], [11., 12., 13.]])]
 
-        net = nn.NeuralNet(weights=array, nr_of_input_nodes=4, hidden_layers=1, hidden_layer_nodes=2, nr_of_outputs=2, recurrence=False)
+        net = nn.NeuralNet(
+            weights=array,
+            nr_of_input_nodes=4,
+            hidden_layers=1,
+            hidden_layer_nodes=2,
+            nr_of_outputs=3,
+            recurrence=False)
+        mat2 = net.weights_as_mat()
+        for i in range(0, len(mat1)):
+            npt.assert_almost_equal(mat1[i], mat2[i])
+
+    def testTranslationWithRecurrence(self):
+        array = [0., 1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11.]
+        mat1 = [np.array([[0., 1.], [2., 3.], [4., 5.], [6., 7.]]), np.array([[8., 9.], [10., 11.]])]
+
+        net = nn.NeuralNet(
+            weights=array,
+            nr_of_input_nodes=2,
+            hidden_layers=1,
+            hidden_layer_nodes=2,
+            nr_of_outputs=2,
+            recurrence=True)
         mat2 = net.weights_as_mat()
         for i in range(0, len(mat1)):
             npt.assert_almost_equal(mat1[i], mat2[i])
@@ -32,21 +52,55 @@ class TestANN(unittest.TestCase):
         flat_list = nn.flatten_weights(mat1)
         self.assertAlmostEqual(array, flat_list)
 
-    def testFWDPROP(self):
+    def testForwardPropagation(self):
         inputs = [1., 1.]
-        nr_of_input_nodes = len(inputs)
-        hidden_layers = 0
-        hidden_layer_nodes = 0
-        nr_of_outputs = 2
         weights = [np.array([[1., 2.], [1., 2.]])]
         weights_flat = nn.flatten_weights(weights)
         expected_output = nn.flatten_weights(np.tanh([2., 4.]))
         net = nn.NeuralNet(weights=weights_flat,
-                           nr_of_input_nodes=nr_of_input_nodes,
-                           hidden_layers=hidden_layers,
-                           hidden_layer_nodes=hidden_layer_nodes,
-                           nr_of_outputs=nr_of_outputs,
+                           nr_of_input_nodes=len(inputs),
+                           hidden_layers=0,
+                           hidden_layer_nodes=0,
+                           nr_of_outputs=2,
                            recurrence=False)
         output = nn.flatten_weights(net.forward_prop(inputs))
         self.assertAlmostEqual(expected_output, output)
 
+    def testForwardPropagationWithHiddenLayer(self):
+        inputs = [1., 1.]
+        weights = [np.array([[1., 2.], [1., 2.]]), np.array([[1., 1.], [1., 1.]])]
+        expected_output = nn.flatten_weights(
+            np.tanh([1.9633568798148839277386570684426, 1.9633568798148839277386570684426]))
+        weights_flat = nn.flatten_weights(weights)
+        net = nn.NeuralNet(weights=weights_flat,
+                           nr_of_input_nodes=len(inputs),
+                           hidden_layers=1,
+                           hidden_layer_nodes=2,
+                           nr_of_outputs=2,
+                           recurrence=False)
+        output = nn.flatten_weights(net.forward_prop(inputs))
+        self.assertAlmostEqual(expected_output, output)
+
+    def testForwardPropagationWithHiddenLayerAndRecurrence(self):
+        inputs = [1., 1.]
+        weights = [np.array([[1., 2.], [1., 2.], [1., 1.], [1., 1.]]), np.array([[1., 1.], [1., 1.]])]
+        weights_flat = nn.flatten_weights(weights)
+        net = nn.NeuralNet(weights=weights_flat,
+                           nr_of_input_nodes=len(inputs),
+                           hidden_layers=1,
+                           hidden_layer_nodes=2,
+                           nr_of_outputs=2,
+                           recurrence=True)
+        expected_output_iteration_1 = nn.flatten_weights(
+            np.tanh([1.9633568798148839277386570684426, 1.9633568798148839277386570684426]))
+        # on the first iteration the memory nodes are initialized with 0
+        output_iteration_1 = nn.flatten_weights(net.forward_prop(inputs))
+        self.assertAlmostEqual(expected_output_iteration_1, output_iteration_1)
+
+        # on the second round the memory nodes the value from the previous iteration
+        expected_output_iteration_2 = nn.flatten_weights(
+            np.tanh([1.99920285031581971304150882377, 1.99920285031581971304150882377]))
+        output_iteration_2 = nn.flatten_weights(net.forward_prop(inputs))
+        print(expected_output_iteration_2)
+        print(output_iteration_2)
+        self.assertAlmostEqual(expected_output_iteration_2, output_iteration_2)
