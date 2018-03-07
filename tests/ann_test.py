8c39cc22a59905b27ea9ca3ec089ddcdda6a7968
Drageset <olve.drageset@gmail.com>
2018-03-07 17:36:11 +0100

    deleted debugging prints

--- tests/ann_test.py
+++ tests/ann_test.py
@@ -14,8 +14,6 @@ class TestANN(unittest.TestCase):
 
         net = nn.NeuralNet(weights=array, nr_of_input_nodes=4, hidden_layers=0, nr_of_outputs=2, recurrence=False)
         mat2 = net.weights_as_mat()
-        print(mat1)
-        print(mat2)
         for i in range(0, len(mat1)):
             npt.assert_almost_equal(mat1[i], mat2[i])
 
@@ -25,8 +23,6 @@ class TestANN(unittest.TestCase):
 
         net = nn.NeuralNet(weights=array, nr_of_input_nodes=4, hidden_layers=1, hidden_layer_nodes=2, nr_of_outputs=2, recurrence=False)
         mat2 = net.weights_as_mat()
-        print(mat1)
-        print(mat2)
         for i in range(0, len(mat1)):
             npt.assert_almost_equal(mat1[i], mat2[i])
 
@@ -34,8 +30,6 @@ class TestANN(unittest.TestCase):
         array = [0., 1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11.]
         mat1 = [np.array([[0., 1., 2., 3.], [4., 5., 6., 7.]]), np.array([[8., 9.], [10., 11.]])]
         flat_list = nn.flatten_weights(mat1)
-        print("FLAT:")
-        print(flat_list)
         self.assertAlmostEqual(array, flat_list)
 
     def testFWDPROP(self):
@@ -54,6 +48,5 @@ class TestANN(unittest.TestCase):
                            nr_of_outputs=nr_of_outputs,
                            recurrence=False)
         output = nn.flatten_weights(net.forward_prop(inputs))
-        print(f"OUTPUT FROM ANN: {output}")
         self.assertAlmostEqual(expected_output, output)
 
