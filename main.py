1c66236902040785b974c2ef1f8ceef3002962a0
Drageset <olve.drageset@gmail.com>
2018-03-07 18:05:54 +0100

    wrote method to calculate some parameter

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 from bot import environment as env
+import gen
+from bot import ann
+
+
+def num_of_weights(nr_of_input_nodes=12,
+                   nr_of_hidden_layers=1,
+                   nr_of_hidden_layer_nodes=6,
+                   nr_of_output_nodes=2,
+                   recurrence=False):
+    nr_of_edges = 0
+    if recurrence:
+        nr_of_input_nodes += nr_of_output_nodes
+    if nr_of_hidden_layers > 0:
+        nr_of_edges += nr_of_input_nodes * nr_of_hidden_layer_nodes
+        for i in range(1, nr_of_hidden_layers):
+            nr_of_edges += nr_of_hidden_layer_nodes ** 2
+        nr_of_edges += nr_of_hidden_layer_nodes * nr_of_output_nodes
+    else:
+        nr_of_edges += nr_of_input_nodes * nr_of_output_nodes
+
+    return nr_of_edges
+
 
 if __name__ == "__main__":
     environment = env.Environment()
@@ -10,3 +32,14 @@ if __name__ == "__main__":
     #print("Simulation fitness result: " + str(environment.simulate(False, 1000, 90)))
 
     # TODO: Start the genetic algorithm
+
+    gene_length = num_of_weights(nr_of_input_nodes=12,
+                   nr_of_hidden_layers=1,
+                   nr_of_hidden_layer_nodes=6,
+                   nr_of_output_nodes=2,
+                   recurrence=False)
+
+    genetic_algorithm = gen.GenAlg(cost_function=environment.simulate,gene_length=gene_length)
+
+    environment.simulate(graphics_enabled=False, time_dilation=10, weights=)
+
