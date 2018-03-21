fedf992732d7ef6110aece424bb0982cef5bbb94
Drageset <olve.drageset@gmail.com>
2018-03-21 18:42:09 +0100

    added some comments to the KF

--- bot/kalman.py
+++ bot/kalman.py
@@ -2,32 +2,43 @@ import numpy as np
 
 __author__ = "Olve Drageset, Andre Gramlich"
 
+''' mew_t_minus_1 is the pose at the previous frame
+ sigma_t_minus_1 is the covariance matrix from the previous frame
+ u_t is the [delta_x, delta_y, delta_theta] since t-1 according to our odometry model
+ z_t is the position as predicted by the sensor data '''
+
 
 def kalman_filter(mew_t_minus_1, sigma_t_minus_1, u_t, z_t):
+    # A_t is the Identity matrix because we have 0 inertia
+    # B_t is the Identity matrix because our input u_t is already the delta in position due to control
+    # C_t is th Identity matrix because we want to make it simple
     A_t = B_t = C_t = np.array([[1, 0, 0],
                                 [0, 1, 0],
                                 [0, 0, 1]])
 
-    # TODO: Set B_t to be 3x2 matrix transforming 2X1 velocity vector to a 3x1 pose vector [x, y, theta]
-
     C_t_transpose = np.transpose(C_t)
 
+    # Q_t is the error in the correction, R_t is the error in the prediction
     Q_t = R_t = np.array([[.04, 0, 0],
                           [0, .04, 0],
                           [0, 0, .04]])
 
     # PREDICTION
+    # Set our predicted position according to our old position, velocity, and estimated change due to control
     mew_bar_t = np.matmul(A_t, mew_t_minus_1) + np.matmul(B_t, u_t)
+    # Set our new covariance matrix based on our estimated error in u_t
     sigma_bar_t = np.matmul(np.matmul(A_t, sigma_t_minus_1), np.transpose(A_t)) + R_t
 
     # CORRECTION
     K_t = np.matmul(
         np.matmul(sigma_bar_t, C_t_transpose),
         np.linalg.inv(np.matmul(np.matmul(C_t, sigma_bar_t), C_t_transpose) + Q_t))
+    # Correct our predicted position due to our sensor observation
     mew_t = mew_bar_t + np.matmul(K_t, z_t - np.matmul(C_t, mew_bar_t))
+    # Correct our covariance matrix due to the difference between our prediction and correction due to sensor data
     sigma_t = np.matmul(np.identity(3) - np.matmul(K_t, C_t), sigma_bar_t)
 
-    return mew_t, sigma_t
+    return mew_t, sigma_t  # Prediction of pose at time t, covariance at time t
 
 
 mew_t_minus_1 = np.array([1, 1, 1])
@@ -35,7 +46,7 @@ sigma_t_minus_1 = np.array([[2, 0, 0],
                             [0, 2, 0],
                             [0, 0, 2]])
 u_t = np.array([1, 1, 1])
-z_t = np.array([1, 1, 1])
+z_t = np.array([2, 2, 1])
 
 mew_t, sigma_t = kalman_filter(mew_t_minus_1, sigma_t_minus_1, u_t, z_t)
 
